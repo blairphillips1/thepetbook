6124c2691ae85f03a228d2ec4d4261e62238a3b8
MattyBrown1 <matthew.brown1@sainsburysbank.co.uk>
2023-11-08 14:46:36 +0000

    add csv for likes

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Application } from 'express';
 
 import cors from 'cors';
-
+import fs from 'fs'
 export const app: Application = express();
 const port = process.env.SERVER_PORT || 8080;
 app.use(cors());
@@ -10,6 +10,25 @@ app.get('/', (req: Request, res: Response) => {
   console.log(req.method);
   res.send('Hello world!');
 });
+
+app.get('/pictures', (req: Request, res: Response) => {
+  console.log(req.method);
+  res.send('');
+})
+
+app.get('/likes', (req: Request, res: Response) => {
+  try {
+    const csvFile = fs.createReadStream(`${process.cwd()}/likes.csv`);
+    res
+      .status(200)
+      .setHeader("Content-Type", "text/csv")
+      .setHeader("Content-Disposition", `attachment; filename=likes.csv`)
+      .send(csvFile);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
 
 // start the express server
 app.listen(port, () => {
